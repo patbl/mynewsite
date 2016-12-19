0c18c40661b71ab0bf6c6d23a18336bf60645efc
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2016-12-18 22:50:34 -0800

    Remove unused Redcarpet gem

--- config.rb
+++ config.rb
@@ -45,9 +45,6 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
